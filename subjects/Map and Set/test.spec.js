740db0666b208dea43153efb7ae1b8f913d53f4f
valterbarros <valterinsidee@gmail.com>
2024-02-06 18:53:06 -0300

    add new test case
    Signed-off-by: valterbarros <valterinsidee@gmail.com>

--- subjects/Map and Set/test.spec.js
+++ subjects/Map and Set/test.spec.js
@@ -22,4 +22,23 @@ describe('Map-set', () => {
       expect(Object.fromEntries(map)).toEqual({name: 'valter', name2: 'valter'});
     });
   });
+
+  describe('set', () => {
+    it('should has only unique keys', () => {
+      const duplicatedArray = [1,1,1,1,2,2,2,3,4,5,6];
+
+      expect([...new Set(duplicatedArray)]).toEqual([1,2,3,4,5,6]);
+    });
+
+    it('should use whatever data type as value', () => {
+      const set = new Set();
+      
+      const operations = () => {
+        set.add({name: 'valter'}, [123]);
+        set.add(() => { console.log(123)}, [123]);
+      }
+      
+      expect(operations).not.toThrowError();
+    });
+  });
 });
