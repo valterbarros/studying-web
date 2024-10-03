996029117de8928eebd4b3a33cffcdf271da0a0a
ValterBarros <valterinside@gmail.com>
2024-10-03 20:11:42 -0300

    add examples for service worker
    Signed-off-by: ValterBarros <valterinside@gmail.com>

--- subjects/Browser/service-workers/Service Workers - Registration, LifeCycle, Events, and Dev Tools/service-worker.js
+++ subjects/Browser/service-workers/Service Workers - Registration, LifeCycle, Events, and Dev Tools/service-worker.js
@@ -7,34 +7,94 @@ function longTask() {
   console.log('finish', Date.now() - date);
 }
 
-self.addEventListener('message', () => {
-  console.log('message');
-  longTask();
+self.addEventListener('message', async (ev) => {
+  
+  const clients = await self.clients.matchAll({includeUncontrolled: true});
+  console.log(ev.source.id);
+
+  switch (ev.data.name) {
+    case 'broadcast':
+      clients.forEach((c) => c.postMessage('I received'));
+
+      break;
+  
+    default:
+      break;
+  }
 });
 
-self.addEventListener('install', (ev) => {
-  const p = new Promise((resolve) => {
-    resolve(1);
-  }).then((arg) => console.log('install', arg));
+const cacheName = `v1-${Date.now()}`;
 
-  ev.waitUntil(p);
+self.addEventListener('install', (ev) => {
+  console.log('install');
+
+  ev.waitUntil(caches.open(cacheName).then(async (cache) => {
+    return cache.addAll([
+      '/',
+      './salluti.jpeg',
+      './example.html',
+      './service-worker.js'
+    ], {ignoreVary: true});
+  }));
 
   self.skipWaiting();
 });
 
-self.addEventListener('activate', () => {
-  console.log('activated');
+const EXPIRE_CACHE_DELAY = 500;
 
-  self.clients.claim();
-});
+function isValid(response) {
+  if (!response) return false;
+
+  const fetched = response.headers.get('sw-fetched-on');
+  console.log('fetched', fetched && parseFloat(fetched) + (EXPIRE_CACHE_DELAY) - Date.now());
+  if (fetched && (parseFloat(fetched) + (EXPIRE_CACHE_DELAY)) > Date.now()) return true;
+  return false;
+}
 
 self.addEventListener('fetch', (e) => {
-  console.log('fetch');
+  e.respondWith(
+    caches.match(e.request, { ignoreVary: true }).then((response) => {
+      if (isValid(response)) {
+        console.log('valid', response);
+        
+        return response.clone();
+      }
+
+      return fetch(e.request).then(async (response) => {
+        if (!e.request.url.includes('chrome')) {
+          const copy = response.clone();
+          const headers = new Headers(copy.headers);
+          headers.append('sw-fetched-on', Date.now())
   
-  // self.clients.matchAll({includeUncontrolled: true}).then(function(clients) {
-  //   clients.forEach(client => {
-  //     console.log('client', client);
-  //   });
-  // });
-  // console.log('fetch',e.request);
+          const cache = await caches.open(cacheName);
+
+          const body = await copy.blob()
+
+          await cache.put(e.request, new Response(body, {
+            status: copy.status,
+            statusText: copy.statusText,
+            headers: headers
+          }));
+        }
+
+        return response.clone();
+      });
+   })
+  )
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('activate');
+  
+  const keepKey = [cacheName];
+
+  event.waitUntil(caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => {
+      if (keepKey.indexOf(key) === -1) {
+        return caches.delete(key);
+      }
+    }));
+  }));
+
+  self.clients.claim();
 });
